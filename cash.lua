a546f634458f09dc69b82cb37184d5dbf4997fa2
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-26 18:31:06 +0100

    Create cash.lua

---
+++ cash.lua
@@ -0,0 +1,10 @@
+game:GetService"RunService".RenderStepped:Connect(function()
+local playerHead = game.Players.LocalPlayer.Character.Head 
+    for i, v in pairs(game:GetService("Workspace").Cash:GetDescendants()) do
+        if v.Name  == "TouchInterest" and v.Parent then 
+            firetouchinterest(playerHead, v.Parent, 0)
+            wait(0.01)
+            firetouchinterest(playerHead, v.Parent, 1)
+      end
+end
+end)
